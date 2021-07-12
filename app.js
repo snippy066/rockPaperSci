73ffe7257625ee30aa7d6c0d26590b81a7f1de47
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-12 22:35:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const compScore=0;
 const userScore_show=document.getElementById("usleb");
 const compScore_show=document.getElementById("coleb");
 
+const scoreBoard_div=document.querySelector(".scbrd");
+const result_div=document.querySelector(".beg");
+
+const rock_i=document.getElementById("r");
+const paper_i=document.getElementById("p");
+const scsr_i=document.getElementById("s");
