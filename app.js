f8904950105cfbead764c2f7af8afe15b5697116
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-12 22:50:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,32 @@ const result_div=document.querySelector(".beg");
 const rock_i=document.getElementById("r");
 const paper_i=document.getElementById("p");
 const scsr_i=document.getElementById("s");
+
+
+function getComputerChoi(){
+    const choices=['r','p','s'];
+
+    const ch=Math.floor(Math.random()*3);
+     return choices[ch];
+}
+
+function game(Choice){
+    const compChoice=getComputerChoi();
+    console.log(Choice);
+    console.log(compChoice);
+}
+
+function main(){
+    rock_i.addEventListener('click' ,function(){
+        game("r");
+    });
+    paper_i.addEventListener('click' ,function(){
+        game("p");
+    });
+    scsr_i.addEventListener('click' ,function(){
+        game("s");
+    });
+
+}
+
+main();
